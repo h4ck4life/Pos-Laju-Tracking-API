c2ef1db6e035f12c9656693e5a20b2fc170aa8ee
alif <alifaziz@gmail.com>
2013-12-13 04:40:52 +0800

    Add cron demo

--- config/init.js
+++ config/init.js
@@ -1,27 +1,54 @@
+var nodemailer = require("nodemailer");
 
 // Add uncaught-exception handler in prod-like environments
-if (geddy.config.environment != 'development') {
-  process.addListener('uncaughtException', function (err) {
-    var msg = err.message;
-    if (err.stack) {
-      msg += '\n' + err.stack;
-    }
-    if (!msg) {
-      msg = JSON.stringify(err);
-    }
-    geddy.log.error(msg);
-  });
+if (geddy.config.environment != "development") {
+    process.addListener("uncaughtException", function(err) {
+        var msg = err.message;
+        if (err.stack) {
+            msg += "\n" + err.stack;
+        }
+        if (!msg) {
+            msg = JSON.stringify(err);
+        }
+        geddy.log.error(msg);
+    });
 }
 
-// Cron job to fetch parcel delivery status
-// var cronJob = require('cron').CronJob;
-// var job = new cronJob('1 1 * * * *', function(){
-//     geddy.log.debug("ALIF IS GREAT");
-//   }, function () {
-//     // This function is executed when the job stops
-//   }, 
-//   true /* Start the job right now */,
-//   "Asia/Kuala_Lumpur" /* Time zone of this job. */
-//  
-// );
+// create reusable transport method (opens pool of SMTP connections)
+var smtpTransport = nodemailer.createTransport("SMTP", {
+    service: "Gmail",
+    auth: {
+        user: "alifaziz@gmail.com",
+        pass: "pranee1989"
+    }
+});
 
+
+// setup e-mail data with unicode symbols
+var mailOptions = {
+    from: "Fred Foo ✔ <foo@blurdybloop.com>", // sender address
+    to: "bar@blurdybloop.com, baz@blurdybloop.com", // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world ✔", // plaintext body
+    html: "<b>Hello world ✔</b>" // html body
+}
+
+
+//Cron job to fetch parcel delivery status
+var cronJob = require("cron").CronJob;
+
+var job = new cronJob("00 30 08 * * 1-5", function() {
+    //geddy.log.debug("ALIF IS GREAT");
+
+    // send mail with defined transport object
+    smtpTransport.sendMail(mailOptions, function(error, response) {
+        if (error) {
+            geddy.log.error(error);
+        } else {
+            geddy.log.info("Message sent: " + response.message);
+        }
+        // if you don't want to use this transport object anymore, uncomment following line
+        //smtpTransport.close(); // shut down the connection pool, no more messages
+    });
+
+}, function() {}, true, "Asia/Kuala_Lumpur");
