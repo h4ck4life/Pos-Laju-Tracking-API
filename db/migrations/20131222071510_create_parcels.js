4c73ae47bdfe47774d6c3a8004578a63d7c5aedf
alif <alifaziz@gmail.com>
2013-12-22 07:15:48 +0800

    new db

---
+++ db/migrations/20131222071510_create_parcels.js
@@ -0,0 +1,33 @@
+var CreateParcels = function () {
+  this.up = function (next) {
+    var def = function (t) {
+          t.column('posid', 'string');
+          t.column('status', 'string');
+          t.column('delivered', 'number');
+          t.column('notifyemail', 'string');
+        }
+      , callback = function (err, data) {
+          if (err) {
+            throw err;
+          }
+          else {
+            next();
+          }
+        };
+    this.createTable('parcel', def, callback);
+  };
+
+  this.down = function (next) {
+    var callback = function (err, data) {
+          if (err) {
+            throw err;
+          }
+          else {
+            next();
+          }
+        };
+    this.dropTable('parcel', callback);
+  };
+};
+
+exports.CreateParcels = CreateParcels;
