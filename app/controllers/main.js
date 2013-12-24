625597193ebacab2c61e3a7e8e826fd3655a7ce1
alif <alifaziz@gmail.com>
2013-12-25 03:31:12 +0800

    Code beautification.

--- app/controllers/main.js
+++ app/controllers/main.js
@@ -7,152 +7,158 @@ var cache = require("memory-cache");
 
 var poslajutracking = require("../../lib/poslajutracking_lib.js");
 
-var Main = function() {
-    //nexmo.initialize("c3f76bb8", "571c5e4a", "http", false);
-    this.index = function(req, resp, params) {
-        if (cache.get("index") == null) {
-            var respondObj = {
-                format: "html",
-                template: "app/views/main/index"
-            };
-            cache.put("index", respondObj);
-            this.respond({title: 'Pos Laju Tracking API | @h4ck4life'}, respondObj);
-        } else {
-            this.respond({title: 'Pos Laju Tracking API | @h4ck4life'}, cache.get("index"));
-        }
-    };
-    this.get = function(req, respo, params) {
-        var self = this;
-        if (cache.get(params.id) == null) {
-            poslajutracking.parseTrackingID(params.id, params.type, this, function(respObj) {
-                cache.put(params.id, respObj, 9e5);
-                if (params.type === "json") {
-                    self.respond(JSON.stringify(respObj), {
-                        format: "js"
-                    });
-                } else {
-                    self.respond(JSON.stringify(respObj), {
-                        format: "txt"
-                    });
-                }
-            });
-        } else {
-            if (params.type === "json") {
-                this.respond(JSON.stringify(cache.get(params.id)), {
-                    format: "js"
-                });
-            } else {
-                this.respond(JSON.stringify(cache.get(params.id)), {
-                    format: "txt"
-                });
-            }
-        }
-    };
-    this.priceDomestic = function(req, respo, params) {
-        var self = this;
-        if (cache.get(params.gram + params.id) == null) {
-            poslajutracking.parseDomesticPricing(params.gram, params.id, params.type, this, function(respObj) {
-                cache.put(params.gram + params.id, respObj, 864e5);
-                if (params.type === "json") {
-                    self.respond(JSON.stringify(respObj), {
-                        format: "js"
-                    });
-                } else {
-                    self.respond(JSON.stringify(respObj), {
-                        format: "txt"
-                    });
-                }
-            });
-        } else {
-            if (params.type === "json") {
-                this.respond(JSON.stringify(cache.get(params.gram + params.id)), {
-                    format: "js"
-                });
-            } else {
-                this.respond(JSON.stringify(cache.get(params.gram + params.id)), {
-                    format: "txt"
-                });
-            }
-        }
-    };
-    this.monitor = function(req, respo, params) {
-        var self = this;
-        var Parcelparams;
-        var origin = req.headers.host;
-        // poslajutracking.herokuapp.com
-        // localhost:4000
-        if (origin !== "poslajutracking.herokuapp.com") {
-            self.respond({
-                saved: false,
-                debug: "You're not allowed to post. Sorry."
-            }, {
-                format: "json"
-            });
-            return false;
-        }
-        if (params.notifyemail && params.id) {
-            geddy.model.Parcel.all({
-                posid: params.id
-            }, function(err, data) {
-                if (data.length > 0) {
-                    self.respond({
-                        saved: false,
-                        debug: "Parcel ID already exists.",
-                        parcelId: params.id
-                    }, {
-                        format: "json"
-                    });
-                    return false;
-                } else {
-                    Parcelparams = {
-                        posid: params.id,
-                        notifyemail: params.notifyemail,
-                        delivered: 0
-                    };
-                    var parcel = geddy.model.Parcel.create(Parcelparams);
-                    if (parcel.isValid()) {
-                        parcel.save(function(err, data) {
-                            if (err) {
-                                self.respond({
-                                    saved: false,
-                                    debug: "Make sure 'id' and 'notifyemail' parameters are present"
-                                }, {
-                                    format: "json"
-                                });
-                                throw err;
-                            }
-                            self.respond({
-                                saved: true
-                            }, {
-                                format: "json"
-                            });
-                            return false;
-                        });
-                    }
-                }
-            });
-        } else {
-            self.respond({
-                saved: false,
-                debug: "Make sure 'id' and 'notifyemail' parameters are present"
-            }, {
-                format: "json"
-            });
-        }
-    };
-    this.notify = function(req, respo, params) {
-        var paramsRender = {title: 'Pos Laju Parcel Status Notifier'};
-        if (cache.get("notify") == null) {
-            var respondObj = {
-                format: "html",
-                template: "app/views/main/notify"
-            };
-            cache.put("notify", respondObj);
-            this.respond(paramsRender, respondObj);
-        } else {
-            this.respond(paramsRender, cache.get("notify"));
-        }
-    };
+var Main = function () {
+	//nexmo.initialize("c3f76bb8", "571c5e4a", "http", false);
+	this.index = function (req, resp, params) {
+		if (cache.get("index") == null) {
+			var respondObj = {
+				format: "html",
+				template: "app/views/main/index"
+			};
+			cache.put("index", respondObj);
+			this.respond({
+				title: 'Pos Laju Tracking API | @h4ck4life'
+			}, respondObj);
+		} else {
+			this.respond({
+				title: 'Pos Laju Tracking API | @h4ck4life'
+			}, cache.get("index"));
+		}
+	};
+	this.get = function (req, respo, params) {
+		var self = this;
+		if (cache.get(params.id) == null) {
+			poslajutracking.parseTrackingID(params.id, params.type, this, function (respObj) {
+				cache.put(params.id, respObj, 9e5);
+				if (params.type === "json") {
+					self.respond(JSON.stringify(respObj), {
+						format: "js"
+					});
+				} else {
+					self.respond(JSON.stringify(respObj), {
+						format: "txt"
+					});
+				}
+			});
+		} else {
+			if (params.type === "json") {
+				this.respond(JSON.stringify(cache.get(params.id)), {
+					format: "js"
+				});
+			} else {
+				this.respond(JSON.stringify(cache.get(params.id)), {
+					format: "txt"
+				});
+			}
+		}
+	};
+	this.priceDomestic = function (req, respo, params) {
+		var self = this;
+		if (cache.get(params.gram + params.id) == null) {
+			poslajutracking.parseDomesticPricing(params.gram, params.id, params.type, this, function (respObj) {
+				cache.put(params.gram + params.id, respObj, 864e5);
+				if (params.type === "json") {
+					self.respond(JSON.stringify(respObj), {
+						format: "js"
+					});
+				} else {
+					self.respond(JSON.stringify(respObj), {
+						format: "txt"
+					});
+				}
+			});
+		} else {
+			if (params.type === "json") {
+				this.respond(JSON.stringify(cache.get(params.gram + params.id)), {
+					format: "js"
+				});
+			} else {
+				this.respond(JSON.stringify(cache.get(params.gram + params.id)), {
+					format: "txt"
+				});
+			}
+		}
+	};
+	this.monitor = function (req, respo, params) {
+		var self = this;
+		var Parcelparams;
+		var origin = req.headers.host;
+		// poslajutracking.herokuapp.com
+		// localhost:4000
+		if (origin !== "poslajutracking.herokuapp.com") {
+			self.respond({
+				saved: false,
+				debug: "You're not allowed to post. Sorry."
+			}, {
+				format: "json"
+			});
+			return false;
+		}
+		if (params.notifyemail && params.id) {
+			geddy.model.Parcel.all({
+				posid: params.id
+			}, function (err, data) {
+				if (data.length > 0) {
+					self.respond({
+						saved: false,
+						debug: "Parcel ID already exists.",
+						parcelId: params.id
+					}, {
+						format: "json"
+					});
+					return false;
+				} else {
+					Parcelparams = {
+						posid: params.id,
+						notifyemail: params.notifyemail,
+						delivered: 0
+					};
+					var parcel = geddy.model.Parcel.create(Parcelparams);
+					if (parcel.isValid()) {
+						parcel.save(function (err, data) {
+							if (err) {
+								self.respond({
+									saved: false,
+									debug: "Make sure 'id' and 'notifyemail' parameters are present"
+								}, {
+									format: "json"
+								});
+								throw err;
+							}
+							self.respond({
+								saved: true
+							}, {
+								format: "json"
+							});
+							return false;
+						});
+					}
+				}
+			});
+		} else {
+			self.respond({
+				saved: false,
+				debug: "Make sure 'id' and 'notifyemail' parameters are present"
+			}, {
+				format: "json"
+			});
+		}
+	};
+	this.notify = function (req, respo, params) {
+		var paramsRender = {
+			title: 'Pos Laju Parcel Status Notifier'
+		};
+		if (cache.get("notify") == null) {
+			var respondObj = {
+				format: "html",
+				template: "app/views/main/notify"
+			};
+			cache.put("notify", respondObj);
+			this.respond(paramsRender, respondObj);
+		} else {
+			this.respond(paramsRender, cache.get("notify"));
+		}
+	};
 };
 
 exports.Main = Main;
--- config/init.js
+++ config/init.js
@@ -6,86 +6,86 @@ var poslajutracking = require("../lib/poslajutracking_lib.js");
 
 // Add uncaught-exception handler in prod-like environments
 if (geddy.config.environment != "development") {
-    process.addListener("uncaughtException", function(err) {
-        var msg = err.message;
-        if (err.stack) {
-            msg += "\n" + err.stack;
-        }
-        if (!msg) {
-            msg = JSON.stringify(err);
-        }
-        geddy.log.error(msg);
-    });
+	process.addListener("uncaughtException", function (err) {
+		var msg = err.message;
+		if (err.stack) {
+			msg += "\n" + err.stack;
+		}
+		if (!msg) {
+			msg = JSON.stringify(err);
+		}
+		geddy.log.error(msg);
+	});
 }
 
 // create reusable transport method (opens pool of SMTP connections)
 var smtpTransport = nodemailer.createTransport("SMTP", {
-    //service: "Gmail",
-    host: "mail.alif.my",
-    // hostname
-    port: 587,
-    // port for secure SMTP
-    auth: {
-        user: "noreply@alif.my",
-        pass: "athirah89"
-    }
+	//service: "Gmail",
+	host: "mail.alif.my",
+	// hostname
+	port: 587,
+	// port for secure SMTP
+	auth: {
+		user: "noreply@alif.my",
+		pass: "athirah89"
+	}
 });
 
 //Cron job to fetch parcel delivery status
 var cronJob = require("cron").CronJob;
 
 // 0 7-18 * * *
-var job = new cronJob("*/30 * * * *", function() {
-    //geddy.log.debug("ALIF IS GREAT");
-    geddy.model.Parcel.all({
-        delivered: 0
-    }, function(err, data) {
-        var parceldata = data;
-        if (err) {
-            throw err;
-        }
-        // this is going to be costly. So... refactoring mgkin diperlukan later.
-        if (parceldata.length > 0) {
-            async.map(parceldata, function(parcelObj, callback) {
-                poslajutracking.parseTrackingID(parcelObj.posid, null, null, function(respObj) {
-                    // if the parcel has any data..
-                    if (respObj.data.length > 0) {
-                        if (parcelObj.status !== respObj.data[0].process) {
-                            // if the parcel successfullt delivered, set the delivered flag to 1.
-                            if (respObj.data[0].process.search("successfully delivered") != -1) {
-                                parcelObj.updateProperties({
-                                    delivered: 1
-                                });
-                            }
-                            // save the current status
-                            parcelObj.updateProperties({
-                                status: respObj.data[0].process
-                            });
-                            //console.log(parcelObj.posid);
-                            parcelObj.save();
-                            // setup e-mail data with unicode symbols
-                            var mailOptions = {
-                                from: "Pos Laju Tracking Service <noreply@alif.my>",
-                                // sender address
-                                //to: "bar@blurdybloop.com, baz@blurdybloop.com", // list of receivers
-                                to: parcelObj.notifyemail,
-                                subject: "Parcel Delivery Status",
-                                // Subject line
-                                // plaintext body
-                                html: "Process: " + respObj.data[0].process + "<br />" + "Office: " + respObj.data[0].office + "<br />" + "Date: " + respObj.data[0].date + "<br />" + "Time: " + respObj.data[0].time
-                            };
-                            // send mail with defined transport object
-                            smtpTransport.sendMail(mailOptions, function(error, response) {
-                                if (error) {
-                                    geddy.log.error("Error: " + error);
-                                } else {}
-                            });
-                        }
-                    }
-                });
-            }, function(err, stats) {
-                if (err) {} else {}
-            });
-        }
-    });
-}, function() {}, true, "Asia/Kuala_Lumpur");
+var job = new cronJob("*/30 * * * *", function () {
+	//geddy.log.debug("ALIF IS GREAT");
+	geddy.model.Parcel.all({
+		delivered: 0
+	}, function (err, data) {
+		var parceldata = data;
+		if (err) {
+			throw err;
+		}
+		// this is going to be costly. So... refactoring mgkin diperlukan later.
+		if (parceldata.length > 0) {
+			async.map(parceldata, function (parcelObj, callback) {
+				poslajutracking.parseTrackingID(parcelObj.posid, null, null, function (respObj) {
+					// if the parcel has any data..
+					if (respObj.data.length > 0) {
+						if (parcelObj.status !== respObj.data[0].process) {
+							// if the parcel successfullt delivered, set the delivered flag to 1.
+							if (respObj.data[0].process.search("successfully delivered") != -1) {
+								parcelObj.updateProperties({
+									delivered: 1
+								});
+							}
+							// save the current status
+							parcelObj.updateProperties({
+								status: respObj.data[0].process
+							});
+							//console.log(parcelObj.posid);
+							parcelObj.save();
+							// setup e-mail data with unicode symbols
+							var mailOptions = {
+								from: "Pos Laju Tracking Service <noreply@alif.my>",
+								// sender address
+								//to: "bar@blurdybloop.com, baz@blurdybloop.com", // list of receivers
+								to: parcelObj.notifyemail,
+								subject: "Parcel Delivery Status",
+								// Subject line
+								// plaintext body
+								html: "Process: " + respObj.data[0].process + "<br />" + "Office: " + respObj.data[0].office + "<br />" + "Date: " + respObj.data[0].date + "<br />" + "Time: " + respObj.data[0].time
+							};
+							// send mail with defined transport object
+							smtpTransport.sendMail(mailOptions, function (error, response) {
+								if (error) {
+									geddy.log.error("Error: " + error);
+								} else {}
+							});
+						}
+					}
+				});
+			}, function (err, stats) {
+				if (err) {} else {}
+			});
+		}
+	});
+}, function () {}, true, "Asia/Kuala_Lumpur");
